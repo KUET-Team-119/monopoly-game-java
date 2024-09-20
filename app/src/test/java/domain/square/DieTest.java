3b66a9c91b87e99a29e31aa116144fbd1e272bd5
gjtmdwns <gjtmdwns0815@naver.com>
2024-09-21 08:45:14 +0900

    TEST : roll 메소드에 대한 테스트 추가

--- app/src/test/java/domain/square/DieTest.java
+++ app/src/test/java/domain/square/DieTest.java
@@ -6,4 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DieTest {
+    @Test
+    @DisplayName("주사위 생성")
+    void 생성() {
+        Die die = new Die("1");
+        assertThat(die).isEqualTo(new Die("1"));
+    }
+
+    @Test
+    @DisplayName("주사위 굴리기")
+    void roll() {
+        Die die = new Die("1");
+        assertThat(die.roll(6)).isEqualTo(6);
+    }
 }
