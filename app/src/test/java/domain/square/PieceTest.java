b6869a45dfaecab7de0ad89489d01f7878d7d17b
gjtmdwns <gjtmdwns0815@naver.com>
2024-09-21 08:50:53 +0900

    TEST : 테스트 실패 -> 성공

--- app/src/test/java/domain/square/PieceTest.java
+++ app/src/test/java/domain/square/PieceTest.java
@@ -1,16 +1,15 @@
-// package domain.square;
+ package domain.square;
 
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.DisplayName;
-// import static org.junit.jupiter.api.Assertions.*;
-// import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+ import org.junit.jupiter.api.Test;
+ import org.junit.jupiter.api.DisplayName;
+ import static org.junit.jupiter.api.Assertions.*;
+ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-// public class PieceTest {
-// @Test
-// @DisplayName("Piece 생성 테스트")
-// void 생성() {
-// Square square = new RegularSquare("1","Go");
-// Piece piece = new Piece(square);
-// assertThat(piece).isEqualTo(new Piece(square));
-// }
-// }
+ public class PieceTest {
+     @Test
+     @DisplayName("Piece 생성 테스트")
+     void 생성() {
+         Piece piece = new Piece("chori");
+         assertThat(piece).isEqualTo(new Piece("chori"));
+     }
+ }
--- app/src/test/java/domain/square/SquareTest.java
+++ app/src/test/java/domain/square/SquareTest.java
@@ -1,22 +1,22 @@
-// package domain.square;
+ package domain.square;
 
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.DisplayName;
-// import static org.junit.jupiter.api.Assertions.*;
-// import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+ import org.junit.jupiter.api.Test;
+ import org.junit.jupiter.api.DisplayName;
+ import static org.junit.jupiter.api.Assertions.*;
+ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-// public class SquareTest {
-// @Test
-// @DisplayName("땅 생성 테스트")
-// void 생성() {
-// Square square = new RegularSquare("1", "서울");
-// assertThat(square).isEqualTo(new RegularSquare("1", "서울"));
-// }
+ public class SquareTest {
+     @Test
+     @DisplayName("땅 생성 테스트")
+         void 생성() {
+         Square square = new RegularSquare(1);
+         assertThat(square).isEqualTo(new RegularSquare(1));
+     }
 
-// @Test
-// @DisplayName("땅 가격 찾기 테스트")
-// void findPrice() {
-// RegularSquare square = new RegularSquare("1", "서울");
-// assertThat(square.findPrice()).isEqualTo(1_400_000);
-// }
-// }
+     @Test
+     @DisplayName("땅 가격 찾기 테스트")
+         void findPrice() {
+         RegularSquare square = new RegularSquare(1);
+         assertThat(square.findPrice()).isEqualTo(5_000);
+     }
+ }
