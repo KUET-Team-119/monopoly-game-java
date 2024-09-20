df83896526f7b3c53857fb4c9f636287c44e3846
gjtmdwns <gjtmdwns0815@naver.com>
2024-09-21 08:44:55 +0900

    REFACTOR : roll 메소드에서 랜덤 수를 파라미터로 받도록 수정

--- app/src/main/java/domain/square/Die.java
+++ app/src/main/java/domain/square/Die.java
@@ -1,19 +1,32 @@
 package domain.square;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class Die {
-    Random random;
     String id;
     int faceValue;
 
     public Die(String id) {
-        random = new Random();
         this.id = id;
     }
 
-    public int roll() {
-        faceValue = random.nextInt(6) + 1;
+    public int roll(int value) {
+        faceValue = value;
         return faceValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Die die))
+            return false;
+        return Objects.equals(id, die.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
