280a5b3df9b22c3521c5cfa2e1858aff8cfe368d
Chori <yidodeveloper@gmail.com>
2024-09-21 01:10:09 +0000

    refactor: 말의 현재 위치를 알아낼 때 메서드 사용

--- app/src/main/java/domain/square/Piece.java
+++ app/src/main/java/domain/square/Piece.java
@@ -13,7 +13,7 @@ public class Piece {
 
     public void move(int numOfMoves) {
         System.out.println("앞으로 " + numOfMoves + "칸 이동합니다.");
-        int currentLocationId = location.getId();
+        int currentLocationId = getLocation().getId();
         int destinationId = (currentLocationId + numOfMoves) % Board.SQUARES_TOTAL;
         this.location = new RegularSquare(destinationId);
         System.out.println(location.getName() +"에 도착했습니다.");
