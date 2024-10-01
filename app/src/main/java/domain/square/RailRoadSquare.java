80efe81723a178da0db21c1f92e575537738dacb
gjtmdwns <gjtmdwns0815@naver.com>
2024-10-01 14:33:12 +0900

    REFACTOR : landedOn 메소드를 PropertySquare에서 상속받도록 변경

--- app/src/main/java/domain/square/RailRoadSquare.java
+++ app/src/main/java/domain/square/RailRoadSquare.java
@@ -4,14 +4,11 @@ import domain.component.property.RailRoadProperty;
 import domain.player.Player;
 
 public class RailRoadSquare extends PropertySquare {
+    private final int RAILROAD_RENT = 25;
 
-    public RailRoadSquare(int id, String name, int price) {
-        this.property = new RailRoadProperty(id, name, price);
+    public RailRoadSquare(int id) {
+        super(id);
+        property = new RailRoadProperty(id, name, id);
     }
 
-    @Override
-    void landedOn(Player player) {
-        // TODO
-    }
-    
 }
