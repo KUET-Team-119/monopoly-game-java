e0a115b76c58960ac37aa4cd625eae53a465bc57
gjtmdwns <gjtmdwns0815@naver.com>
2024-10-10 09:28:07 +0900

    FEAT : 은행에 일정 금액을 지불하는 찬스 카드

--- app/src/main/java/domain/component/card/PayChanceCard.java
+++ app/src/main/java/domain/component/card/PayChanceCard.java
@@ -1,2 +1,28 @@
-package domain.component.card;public class PayChanceCard {
+package domain.component.card;
+
+import domain.MonopolyGame;
+import domain.player.Player;
+import monopolygame.App;
+
+import java.util.List;
+
+public class PayChanceCard extends ChanceCard {
+
+    private int payment;
+
+    public PayChanceCard(int payment) {
+        super();
+        this.payment = payment;
+    }
+
+    @Override
+    void takeEffect(Player player) {
+        List<Player> players = MonopolyGame.getPlayers();
+        players.remove(player);
+
+        for (Player otherPlayer : players) {
+            player.reduceCash(payment);
+            otherPlayer.addCash(payment);
+        }
+    }
 }
