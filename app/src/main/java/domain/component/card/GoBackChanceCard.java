2cf32bbb3e106136496b38f205e8e48b0e4222a5
gjtmdwns <gjtmdwns0815@naver.com>
2024-10-10 09:27:48 +0900

    FEAT : 뒤로 3칸 이동하는 찬스 카드

--- app/src/main/java/domain/component/card/GoBackChanceCard.java
+++ app/src/main/java/domain/component/card/GoBackChanceCard.java
@@ -1,10 +1,18 @@
 package domain.component.card;
 
+import domain.component.Board;
+import domain.component.Piece;
 import domain.player.Player;
 
 public class GoBackChanceCard extends ChanceCard {
+
+    private final int BACK = 3;
+
     @Override
     void takeEffect(Player player) {
+        Piece piece = player.getPiece();
+        int currentLocationId = piece.getLocation().getId();
 
+        piece.setLocation(Board.squares.get(currentLocationId - BACK));
     }
 }
