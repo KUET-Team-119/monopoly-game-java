741a0d0bbd93803ec7443a4d76a299da854297b0
gjtmdwns <gjtmdwns0815@naver.com>
2024-10-10 10:47:48 +0900

    FIX : UtilitySquare의 클래스를 반환하도록 변경

--- app/src/main/java/domain/component/card/ForwardToNearestUtilitySquareChanceCard.java
+++ app/src/main/java/domain/component/card/ForwardToNearestUtilitySquareChanceCard.java
@@ -2,28 +2,18 @@ package domain.component.card;
 
 import domain.component.Board;
 import domain.player.Player;
+import domain.square.RailRoadSquare;
 import domain.square.Square;
 import domain.square.SquareType;
 import domain.square.UtilitySquare;
 
-public class ForwardToNearestUtilitySquareChanceCard extends ChanceCard {
+public class ForwardToNearestUtilitySquareChanceCard extends ForwardToNearestSquareChanceCard {
 
     private final int GO_SQUARE_INDEX = SquareType.GO.getIndex();
 
     @Override
-    void takeEffect(Player player) {
-        int destinationId = 0;
-        int currentLocationId = player.getPiece().getLocation().getId();
-
-        for (Square square : Board.squares.values()) {
-            if (square.getId() < currentLocationId) {
-                continue;
-            }
-            if (square instanceof UtilitySquare) {
-                destinationId = square.getId();
-                break;
-            }
-        }
-        player.getPiece().setLocation(Board.squares.get(destinationId));
+    protected Class<? extends Square> getTargetSquareClass() {
+        return UtilitySquare.class;
     }
+
 }
