82fa9141a5c84e4fa37098052c94daf5a480a9e4
gjtmdwns <gjtmdwns0815@naver.com>
2024-10-10 09:26:25 +0900

    FEAT : 가장 가까운 철도 칸으로 이동하는 찬스 카드

--- app/src/main/java/domain/component/card/ForwardToNearestRailRoadSquareChanceCard.java
+++ app/src/main/java/domain/component/card/ForwardToNearestRailRoadSquareChanceCard.java
@@ -2,16 +2,28 @@ package domain.component.card;
 
 import domain.component.Board;
 import domain.player.Player;
+import domain.square.RailRoadSquare;
+import domain.square.Square;
 import domain.square.SquareType;
 
-public class ForwardToFirstRailRoadSquareChanceCard extends ChanceCard {
+public class ForwardToNearestRailRoadSquareChanceCard extends ChanceCard {
 
     private final int GO_SQUARE_INDEX = SquareType.GO.getIndex();
 
     @Override
     void takeEffect(Player player) {
+        int destinationId = 0;
         int currentLocationId = player.getPiece().getLocation().getId();
-        // 항상 출발점에 도착하는지 확인 필요
-        player.getPiece().goForward(Board.SQUARES_TOTAL - currentLocationId);
+
+        for (Square square : Board.squares.values()) {
+            if (square.getId() < currentLocationId) {
+                continue;
+            }
+            if (square instanceof RailRoadSquare) {
+                destinationId = square.getId();
+                break;
+            }
+        }
+        player.getPiece().setLocation(Board.squares.get(destinationId));
     }
 }
