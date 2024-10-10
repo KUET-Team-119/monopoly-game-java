43503372fda91a294c7ece6f9629028d20dcaea7
gjtmdwns <gjtmdwns0815@naver.com>
2024-10-10 10:47:25 +0900

    FIX : RailRoadSquare의 클래스를 반환하도록 변경

--- app/src/main/java/domain/component/card/ForwardToNearestRailRoadSquareChanceCard.java
+++ app/src/main/java/domain/component/card/ForwardToNearestRailRoadSquareChanceCard.java
@@ -5,25 +5,15 @@ import domain.player.Player;
 import domain.square.RailRoadSquare;
 import domain.square.Square;
 import domain.square.SquareType;
+import domain.square.UtilitySquare;
 
-public class ForwardToNearestRailRoadSquareChanceCard extends ChanceCard {
+public class ForwardToNearestRailRoadSquareChanceCard extends ForwardToNearestSquareChanceCard {
 
     private final int GO_SQUARE_INDEX = SquareType.GO.getIndex();
 
     @Override
-    void takeEffect(Player player) {
-        int destinationId = 0;
-        int currentLocationId = player.getPiece().getLocation().getId();
-
-        for (Square square : Board.squares.values()) {
-            if (square.getId() < currentLocationId) {
-                continue;
-            }
-            if (square instanceof RailRoadSquare) {
-                destinationId = square.getId();
-                break;
-            }
-        }
-        player.getPiece().setLocation(Board.squares.get(destinationId));
+    protected Class<? extends Square> getTargetSquareClass() {
+        return RailRoadSquare.class;
     }
+
 }
