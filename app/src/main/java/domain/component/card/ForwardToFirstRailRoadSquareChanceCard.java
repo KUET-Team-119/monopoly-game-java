924ddf4cf061a6b6f1a81689b31d01b84317b951
gjtmdwns <gjtmdwns0815@naver.com>
2024-10-10 09:21:04 +0900

    FEAT : 첫 철도 칸(광주역)으로 이동하는 찬스카드

--- app/src/main/java/domain/component/card/ForwardToFirstRailRoadSquareChanceCard.java
+++ app/src/main/java/domain/component/card/ForwardToFirstRailRoadSquareChanceCard.java
@@ -4,14 +4,12 @@ import domain.component.Board;
 import domain.player.Player;
 import domain.square.SquareType;
 
-public class ForwardToGoSquareChanceCard extends ChanceCard {
-
-    private final int GO_SQUARE_INDEX = SquareType.GO.getIndex();
+public class ForwardToFirstRailRoadSquareChanceCard extends ChanceCard {
 
     @Override
     void takeEffect(Player player) {
         int currentLocationId = player.getPiece().getLocation().getId();
-        // 항상 출발점에 도착하는지 확인 필요
-        player.getPiece().goForward(Board.SQUARES_TOTAL - currentLocationId);
+        // 항상 광주역에 도착하는지 확인 필요
+        player.getPiece().goForward(Board.SQUARES_TOTAL - currentLocationId + SquareType.RAILROAD_GWANGJU.getIndex());
     }
 }
