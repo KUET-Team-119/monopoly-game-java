f2503a462f3a202fd2ca903080e4ec0688b1779a
gjtmdwns <gjtmdwns0815@naver.com>
2024-10-10 09:21:37 +0900

    FEAT : 출발칸으로 이동하는 찬스카드

--- app/src/main/java/domain/component/card/ForwardToGoSquareChanceCard.java
+++ app/src/main/java/domain/component/card/ForwardToGoSquareChanceCard.java
@@ -1,2 +1,17 @@
-package domain.component.card;public class ForwardToGoSquareChanceCard {
+package domain.component.card;
+
+import domain.component.Board;
+import domain.player.Player;
+import domain.square.SquareType;
+
+public class ForwardToGoSquareChanceCard extends ChanceCard {
+
+    private final int GO_SQUARE_INDEX = SquareType.GO.getIndex();
+
+    @Override
+    void takeEffect(Player player) {
+        int currentLocationId = player.getPiece().getLocation().getId();
+        // 항상 출발점에 도착하는지 확인 필요
+        player.getPiece().goForward(Board.SQUARES_TOTAL - currentLocationId);
+    }
 }
