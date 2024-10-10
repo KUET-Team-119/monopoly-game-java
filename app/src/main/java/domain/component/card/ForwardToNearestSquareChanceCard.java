4b161b84de78c1ce5c34793cdb97a0b8efc621b8
gjtmdwns <gjtmdwns0815@naver.com>
2024-10-10 10:46:50 +0900

    FEAT : 가장 가까운 유틸리티 OR 철도 칸으로 이동하는 로직이 비슷하므로 ForwardToNearestSquareChanceCard 추상 클래스로 묶어주기

---
+++ app/src/main/java/domain/component/card/ForwardToNearestSquareChanceCard.java
@@ -0,0 +1,45 @@
+package domain.component.card;
+
+import domain.component.Board;
+import domain.player.Player;
+import domain.square.PropertySquare;
+import domain.square.Square;
+import domain.square.UtilitySquare;
+
+public abstract class ForwardToNearestSquareChanceCard extends ChanceCard {
+
+    protected abstract Class<? extends Square> getTargetSquareClass();
+
+    @Override
+    void takeEffect(Player player) {
+        int currentLocationId = player.getPiece().getLocation().getId();
+        Class<? extends Square> targetSquare = getTargetSquareClass();
+
+        int destinationId = -1;
+
+        for (int i = currentLocationId + 1; i < Board.squares.size(); i++) {
+            Square square = Board.squares.get(i);
+            if (targetSquare.isInstance(square)) {
+                destinationId = square.getId();
+                break;
+            }
+        }
+
+        if (destinationId != -1) {
+            player.getPiece().setLocation(Board.squares.get(destinationId));
+            return;
+        }
+
+        // 유틸리티 칸이 없다면, 보드 처음부터 탐색
+        for (int i = 0; i <= currentLocationId; i++) {
+            Square square = Board.squares.get(i);
+            if (targetSquare.isInstance(square)) {
+                destinationId = square.getId();
+                break;
+            }
+        }
+
+        player.getPiece().setLocationAndReceiveSalary(Board.squares.get(destinationId));
+    }
+
+}
