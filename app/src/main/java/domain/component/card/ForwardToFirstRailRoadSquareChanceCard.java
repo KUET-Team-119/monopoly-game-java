015bffefa206a55cba025f5953e09b25a65f667f
pakaheo <k70923@konkuk.ac.kr>
2024-10-14 11:39:45 +0900

    REFACTOR : player에게 전진 요청하도록 변경

--- app/src/main/java/domain/component/card/ForwardToFirstRailRoadSquareChanceCard.java
+++ app/src/main/java/domain/component/card/ForwardToFirstRailRoadSquareChanceCard.java
@@ -8,8 +8,6 @@ public class ForwardToFirstRailRoadSquareChanceCard extends ChanceCard {
 
     @Override
     void takeEffect(Player player) {
-        int currentLocationId = player.getPiece().getLocation().getId();
-        // 항상 광주역에 도착하는지 확인 필요
-        player.getPiece().goForward(Board.SQUARES_TOTAL - currentLocationId + SquareType.RAILROAD_GWANGJU.getIndex());
+        player.askForGoForward(SquareType.RAILROAD_GWANGJU);
     }
 }
